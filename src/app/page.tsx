e1865795a8f00f283d88b1d09dc80f4c17eead6a
Marcelo da Silva <marceloxhenrique@hotmail.com>
2025-08-06 23:33:14 +0200

    feat: add ChatBox component to display user/system messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import ChatBox from "../components/chat-box";
 import { ingest } from "../ingest/ingest";
 export default function Home() {
   ingest();
-  return <main className="container mx-auto "></main>;
+  return (
+    <main className="container mx-auto">
+      <ChatBox></ChatBox>
+    </main>
+  );
 }
